af5498cd1214cc59a35ab597dc311524a63b4c96
Michał Żuk <michal.zuk30601@gmail.com>
2024-08-10 17:10:34 +0200

    Add project files.

---
+++ TestMaker.Hybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMaker.Hybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
