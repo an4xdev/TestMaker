c5ab4d9233b6b495e11975b15d59c35a0b64d98a
Michał Żuk <michal.zuk30601@gmail.com>
2024-08-11 19:26:31 +0200

    project and question model, project refernces

---
+++ TestMaker.Data/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace TestMaker.Data;
+
+public class Project
+{
+    public string Name { get; set; } = string.Empty;
+
+    public List<Question> Questions { get; set; } = [];
+}
---
+++ TestMaker.Data/Question.cs
@@ -0,0 +1,7 @@
+﻿namespace TestMaker.Data;
+
+public abstract class Question
+{
+    public Guid ID { get; set; }
+    public string QuestionText { get; set; } = string.Empty;
+}
