75741b716ebd54f645228e9291f00b0d93ccfa91
Michał Żuk <michal.zuk30601@gmail.com>
2024-08-23 13:07:10 +0200

    modal for configuring generated page

---
+++ TestMaker.Data/Services/ServiceModels/Language.cs
@@ -0,0 +1,7 @@
+﻿namespace TestMaker.Data.Services.ServiceModels;
+
+public class Language
+{
+    public string Code { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
--- TestMaker.Data/Services/ServiceModels/PageContent.cs
+++ TestMaker.Data/Services/ServiceModels/PageContent.cs
@@ -13,6 +13,7 @@ public class PageContent
                 Button4 = "Random multiple choice question";
                 Button5 = "Another open question";
                 Button6 = "Random open question";
+                ShowAnswer = "Show answer";
                 break;
             case Languages.Polish:
                 Button1 = "Kolejne pytanie jednokrotnego wyboru";
@@ -21,21 +22,43 @@ public class PageContent
                 Button4 = "Losowe pytanie wielokrotnego wyboru";
                 Button5 = "Kolejne pytanie otwarte";
                 Button6 = "Losowe pytanie otwarte";
+                ShowAnswer = "Pokaż odpowiedź";
+                break;
+            case Languages.None:
+                Button1 = string.Empty;
+                Button2 = string.Empty;
+                Button3 = string.Empty;
+                Button4 = string.Empty;
+                Button5 = string.Empty;
+                Button6 = string.Empty;
+                ShowAnswer = string.Empty;
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(language), language, null);
         }
     }
-    public string Button1 { get;}
-    public string Button2 { get;}
-    public string Button3 { get;}
-    public string Button4 { get;}
-    public string Button5 { get;}
-    public string Button6 { get;}
+    public string Button1 { get; set;}
+    public string Button2 { get; set;}
+    public string Button3 { get; set;}
+    public string Button4 { get; set;}
+    public string Button5 { get; set;}
+    public string Button6 { get; set;}
+    
+    public string ShowAnswer { get; set; }
+    
+    public bool IsEmpty => string.IsNullOrEmpty(Button1) 
+                              || string.IsNullOrEmpty(Button2) 
+                              || string.IsNullOrEmpty(Button3) 
+                              || string.IsNullOrEmpty(Button4) 
+                              || string.IsNullOrEmpty(Button5) 
+                              || string.IsNullOrEmpty(Button6) 
+                              || string.IsNullOrEmpty(ShowAnswer);
+    
 }
 
 public enum Languages
 {
+    None,
     English,
     Polish
 }
