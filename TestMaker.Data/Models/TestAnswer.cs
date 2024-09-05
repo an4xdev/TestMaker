cfd35634b56fee152447d48bcf0f7c6d1aeb72a6
Michał Żuk <michal.zuk30601@gmail.com>
2024-09-05 21:09:57 +0200

    deleting answers in test questions, decreasing correct answer

--- TestMaker.Data/Models/TestAnswer.cs
+++ TestMaker.Data/Models/TestAnswer.cs
@@ -2,12 +2,14 @@
 
 public class TestAnswer : ICloneable
 {
+    public Guid Id { get; init; } = Guid.NewGuid();
     public string Answer { get; set; } = string.Empty;
     public CorrectAnswer AnswerValue { get; set; }
     public object Clone()
     {
         return new TestAnswer
         {
+            Id = Id,
             Answer = Answer,
             AnswerValue = AnswerValue,
         };
